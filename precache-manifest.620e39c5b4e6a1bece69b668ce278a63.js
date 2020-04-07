4a667ee1ce77dd50015de40a543694eaa332b174
Harry Chiu <harrychiu1106@gmail.com>
2020-04-07 11:09:53 +0800

    Updates

---
+++ precache-manifest.620e39c5b4e6a1bece69b668ce278a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c562b744d8f9e97ac4be60fa810cc3",
+    "url": "/react-e-commerce/index.html"
+  },
+  {
+    "revision": "ada40bd1684bb5f50281",
+    "url": "/react-e-commerce/static/js/2.055a8290.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-e-commerce/static/js/2.055a8290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7270cd46698fa76eac9c",
+    "url": "/react-e-commerce/static/js/main.fa7a493f.chunk.js"
+  },
+  {
+    "revision": "bd59af08b9993ff88549",
+    "url": "/react-e-commerce/static/js/runtime-main.c0822b49.js"
+  }
+]);
